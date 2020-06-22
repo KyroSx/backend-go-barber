48d6e3978d5b7cc2d819e5aa37b23d0eee077d79
leonardo <leolub150@gmail.com>
2020-06-22 13:18:45 -0300

    enable Create Appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,41 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { parseISO, startOfHour, isEqual } from 'date-fns';
 
 const appointmentsRoutes = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Array<Appointment> = [];
 
 appointmentsRoutes.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentsInSameDate = appointments.find(appointment =>
+    isEqual(appointment.date, parsedDate),
+  );
+
+  if (findAppointmentsInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'This appointments is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
-  return response.json(appointment);
+  appointments.push(appointment);
+
+  return response.json(appointments);
 });
 
 export default appointmentsRoutes;
