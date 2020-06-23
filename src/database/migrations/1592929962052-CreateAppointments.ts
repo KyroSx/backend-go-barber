bd3445f9c708865fb4f7de43b4213abd534c1e59
leonardo <leolub150@gmail.com>
2020-06-23 14:53:48 -0300

    is nullable is default

--- src/database/migrations/1592929962052-CreateAppointments.ts
+++ src/database/migrations/1592929962052-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1592929962052
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
