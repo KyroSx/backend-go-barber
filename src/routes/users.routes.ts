aa03cf437f81caebd5c324136c00ea5351bd0d92
leonardo <leolub150@gmail.com>
2020-06-24 10:22:40 -0300

    add password encryption

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,6 +10,8 @@ usersRoutes.post('/', async (request, response) => {
 
     const user = await createUser.execute({ name, email, password });
 
+    delete user.password;
+
     return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
--- src/service/CreateUserService.ts
+++ src/service/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -17,10 +18,12 @@ class CreateUserService {
 
     if (checkUserExists) throw new Error('Email already exists');
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
