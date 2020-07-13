2f825ae5c215f48421091b5c35df5536608932e3
leonardo <leonardo@pop-os.localdomain>
2020-07-13 20:36:38 -0300

    inject dependencion

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,16 @@
 import AppError from '@shared/errors/AppError';
 import FakeUserRepository from '../repositories/fakes/UsersRepository';
 import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 describe('Create User', () => {
   it('should be able to create a new user ', async () => {
     const fakeUserRepository = new FakeUserRepository();
-    const createUserService = new CreateUserService(fakeUserRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
 
     const userCreated = await createUserService.execute({
       name: 'name',
@@ -18,7 +23,11 @@ describe('Create User', () => {
 
   it("should not be able to create a new user with another user's email", async () => {
     const fakeUserRepository = new FakeUserRepository();
-    const createUserService = new CreateUserService(fakeUserRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
 
     const userEmail = 'email@mail.com';
 
