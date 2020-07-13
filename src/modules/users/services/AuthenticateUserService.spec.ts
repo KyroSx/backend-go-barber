5ffc5b174a2a6698f627d4a35120382b8e9f4a4f
leonardo <leonardo@pop-os.localdomain>
2020-07-13 20:37:04 -0300

    ensure User with correct data can be authenticated

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,64 @@
+import AppError from '@shared/errors/AppError';
+import FakeUserRepository from '../repositories/fakes/UsersRepository';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+describe('Authenticate User Service', () => {
+  it('should authenticate user if correct data are provided', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakerHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakerHashProvider,
+    );
+
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUserRepository,
+      fakerHashProvider,
+    );
+
+    const user = await createUserService.execute({
+      name: 'name',
+      email: 'email@mail.com',
+      password: 'password',
+    });
+
+    const response = await authenticateUserService.execute({
+      email: 'email@mail.com',
+      password: 'password',
+    });
+
+    expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+
+  it('should not authenticate user if wrong data are provided', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakerHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakerHashProvider,
+    );
+
+    const authenticateUserService = new AuthenticateUserService(
+      fakeUserRepository,
+      fakerHashProvider,
+    );
+
+    await createUserService.execute({
+      name: 'name',
+      email: 'email@mail.com',
+      password: 'password',
+    });
+
+    const response = authenticateUserService.execute({
+      email: 'invalid_email@mail.com',
+      password: 'password',
+    });
+
+    expect(response).rejects.toBeInstanceOf(AppError);
+  });
+});
