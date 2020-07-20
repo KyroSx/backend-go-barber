04913ae52a45669f5f623fc2a69df13d2fd480fb
leonardo <leonardo@pop-os.localdomain>
2020-07-20 16:21:16 -0300

    ensure User should not be able to reset the password without a non-existing user or token

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -1,27 +1,37 @@
 import 'reflect-metadata';
 
+import AppError from '@shared/errors/AppError';
 import ResetPasswordService from './ResetPasswordService';
 import FakeUsersTokenRepository from '../repositories/fakes/FakeUsersTokenRepository';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 
 const makeSut = () => {
+  const fakeHashProvider = new FakeHashProvider();
   const fakeUsersRepository = new FakeUsersRepository();
   const fakeUsersTokenRepository = new FakeUsersTokenRepository();
   const sut = new ResetPasswordService(
     fakeUsersRepository,
     fakeUsersTokenRepository,
+    fakeHashProvider,
   );
 
   return {
     sut,
     fakeUsersTokenRepository,
     fakeUsersRepository,
+    fakeHashProvider,
   };
 };
 
 describe('Reset Password Service', () => {
   it('should be able to reset the password', async () => {
-    const { sut, fakeUsersRepository, fakeUsersTokenRepository } = makeSut();
+    const {
+      sut,
+      fakeUsersRepository,
+      fakeUsersTokenRepository,
+      fakeHashProvider,
+    } = makeSut();
 
     const user = await fakeUsersRepository.create({
       name: 'name',
@@ -30,11 +40,33 @@ describe('Reset Password Service', () => {
     });
 
     const { token } = await fakeUsersTokenRepository.generate(user.id);
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash');
 
     await sut.execute({ token, password: '123456' });
 
     const userWithPasswordReseted = await fakeUsersRepository.findById(user.id);
 
+    expect(generateHash).toHaveBeenCalledWith('123456');
     expect(userWithPasswordReseted?.password).toBe('123456');
+  });
+
+  it('should not be able to reset the password without a non-existing token', async () => {
+    const { sut } = makeSut();
+
+    await expect(
+      sut.execute({ token: 'non_existing_token', password: '123456' }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to reset the password without a non-existing user', async () => {
+    const { sut, fakeUsersTokenRepository } = makeSut();
+
+    const { token } = await fakeUsersTokenRepository.generate(
+      'non_existing_user_id',
+    );
+
+    await expect(
+      sut.execute({ token, password: '123456' }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUsersTokenRepository from '../repositories/IUserTokenRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   token: string;
@@ -16,6 +17,9 @@ class ResetPasswordService {
 
     @inject('UsersTokenRepository')
     private usersTokenRepository: IUsersTokenRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ token, password }: IRequest): Promise<void> {
@@ -31,7 +35,7 @@ class ResetPasswordService {
       throw new AppError('User does not exits');
     }
 
-    user.password = password;
+    user.password = await this.hashProvider.generateHash(password);
 
     await this.usersRepository.save(user);
   }
