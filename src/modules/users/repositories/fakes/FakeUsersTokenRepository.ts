644a6f782035e79842406dc9b47f190f45c3c03b
leonardo <leonardo@pop-os.localdomain>
2020-07-20 13:09:18 -0300

    create Token for User

---
+++ src/modules/users/repositories/fakes/FakeUsersTokenRepository.ts
@@ -0,0 +1,25 @@
+import { uuid } from 'uuidv4';
+import IUsersTokenRepository from '../IUserTokenRepository';
+import UserToken from '../../infra/typeorm/entities/UserToken';
+
+class FakeUsersTokenRepository implements IUsersTokenRepository {
+  private userTokens: Array<UserToken> = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    const objectToBeAssigned = {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    };
+
+    Object.assign(userToken, objectToBeAssigned);
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export default FakeUsersTokenRepository;
